3811a1c06a62ee09eba4ed44a4e9388d180b29c1
Michael DAgostino <dagostinomichael00@gmail.com>
2021-10-13 21:37:49 -0400

    Updated Main Camera with CameraMotor script

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.4f;
+    public float boundY = 0.2f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // check to see if inside camera bounds on X axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            } 
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        // check to see if inside camera bounds on Y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+
+    }
+
+}
