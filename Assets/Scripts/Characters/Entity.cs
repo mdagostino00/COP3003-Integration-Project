6fb6223e1fa619299193a3b22030faa141eb2dca
Michael DAgostino <dagostinomichael00@gmail.com>
2021-10-30 22:13:09 -0400

    Renamed TestBase to Entity, cleaned up Entity Inheritance, Enemy now tracks and chases an Entity correctly

--- Assets/Scripts/Characters/Entity.cs
+++ Assets/Scripts/Characters/Entity.cs
@@ -1,48 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+//elijah made this by just conglomerating functions from other classes into a useful base class
 
-public class EntityObject : MonoBehaviour
+
+public class Entity : MonoBehaviour
 {
-    // base health and magic
+    // fields
+    protected Vector2 direction;
+    protected Vector2 movement;
+    protected Rigidbody2D body;
+    protected Animator anim;
+
+    [SerializeField]
+    protected float movementSpeed;
+
+    // base health
     [SerializeField]
     protected static int HEALTH_BASE = 100;
     [SerializeField]
-    protected static int MAGICPOINTS_BASE = 20;
-
-    // player's changing health and magic total
-    [SerializeField]
     private int healthTotal = HEALTH_BASE;
     [SerializeField]
-    private int magicPointsTotal = MAGICPOINTS_BASE;
-    
-    // important health bar stuff
     private int health;
-    private int magicPoints;
-    private int inventorySlots;
 
-    // possible skill point based level-up system
-    // private int skillPoints;
-    // private int attack;
-    // private int defense;
-    // private int speed;
-    // private int magic;
-
-    // leveling stuff
     [SerializeField]
     private int level = 1;
-    private int experiencePoints = 0;
 
     // passive modifiers
-    //[SerializeField]
-    private float walkSpeedMultiplier = 1.0f; // how fast player should walk
+    [SerializeField]
+    private float walkSpeedMultiplier = 1.0f; // how fast entity should walk
     //[SerializeField]
     // private float runSpeedMultiplier = 1.2f; // runSpeed for when it's implemented
-    //[SerializeField]
+    [SerializeField]
     private float attackMod = 1.0f; // mod for physical attacks
     //[SerializeField]
     // private float magicMod = 1.0f; // mod for magic-based attacks
-    //[SerializeField]
+    [SerializeField]
     private float defenseMod = 1.0f; // physical defense modifier
     //[SerializeField]
     // private float defenseModMagic = 1.0f; // possible alt defmod for magic-property attacks
@@ -50,14 +44,74 @@ public class EntityObject : MonoBehaviour
     //private float attackSpeed; // how long until player can attack again
 
     public int Health { get => health; set => health = value; }
-    public int MagicPoints { get => magicPoints; set => magicPoints = value; }
-    public float SpeedMultiplier { get => walkSpeedMultiplier; set => walkSpeedMultiplier = value; }
     public int HealthTotal { get => healthTotal; set => healthTotal = value; }
-    public int MagicPointsTotal { get => magicPointsTotal; set => magicPointsTotal = value; }
+    public float SpeedMultiplier { get => walkSpeedMultiplier; set => walkSpeedMultiplier = value; }
     public int Level { get => level; set => level = value; }
     public float DefenseMod { get => defenseMod; set => defenseMod = value; }
-    public int ExperiencePoints { get => experiencePoints; set => experiencePoints = value; }
     public float AttackMod { get => attackMod; set => attackMod = value; }
+
+    // Awake is called when Unity creates the object
+    protected virtual void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // FixedUpdate is called at a fixed interval, not always once per frame.
+    protected virtual void FixedUpdate()
+    {
+        RotateTowardDirection();
+        Movement();
+        Debug.Log("This won't print if override is successful.");
+    }
+
+    // gets the magnitude of the velocity vector
+    float GetMagnitude()
+    {
+        float velocityX = body.velocity.x * body.velocity.x;
+        float velocityY = body.velocity.y * body.velocity.y;
+        float magnitude = Mathf.Sqrt(velocityX + velocityY);
+        return magnitude;
+    }
+
+    protected void Movement()
+    {
+        // get current position
+        Vector2 currentPos = body.position;
+        // calculate move delta
+        Vector2 adjustedMovement = movement * movementSpeed;
+        // add move delta to current position
+        Vector2 newPos = currentPos + adjustedMovement * Time.fixedDeltaTime;
+        // move player to new position
+        body.MovePosition(newPos);
+    }
+
+    protected void Movement(Vector2 direction)
+    {
+        body.MovePosition((Vector2)transform.position + (direction * movementSpeed * Time.deltaTime));
+    }
+
+    protected virtual void RotateTowardDirection() // virtual because we might need to handle this function depending on enemy
+    {
+        //turn off walking
+        if (movement != Vector2.zero) // if we have any movement
+        {
+            // rotate sprite to face direction of movement using quaterions
+            transform.rotation =
+                Quaternion.LookRotation(Vector3.forward, movement);
+        }
+    }
+
+    protected void RotateTowardDirection(ref Vector2 direction)
+    {
+        //rotate the sprite without quaterions (rotation is off by 90)
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //body.rotation = angle;
+
+        //rotate the sprite with quaterions (very cool!)
+        transform.rotation =
+                Quaternion.LookRotation(Vector3.forward, movement);
+    }
 
     public int DamageCalculate(int baseDamage)
     {
@@ -111,7 +165,8 @@ public class EntityObject : MonoBehaviour
                 Debug.Log("This healing item doesn't have a healType\n");
                 break;
         }
-        if (healValue < 1) {  // minimum healing value = 1
+        if (healValue < 1)
+        {  // minimum healing value = 1
             healValue = 1;
             Debug.Log("This healing item healed for less than 1 HP\n");
         }
