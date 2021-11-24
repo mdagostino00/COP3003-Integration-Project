a7ad0479574200536e95ce1ea24d5d487dc482f2
Elijah Nieves <elijahnieves328@gmail.com>
2021-11-24 00:33:20 -0500

    Resolving conflicts the good ole fashioned way

--- Assets/Scripts/Characters/Enemy/Slime.cs
+++ Assets/Scripts/Characters/Enemy/Slime.cs
@@ -1,11 +1,10 @@
-
-// Elijah Nieves
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Slime : Enemy
-{ 
+{
+    
     protected override void Awake()
     {
         base.Awake();
